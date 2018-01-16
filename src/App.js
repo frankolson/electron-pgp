906f9bb728934fcdd1294ad18dff113106afb140
Will Olson <will.olson11@gmail.com>
2018-01-16 09:10:43 -0800

    feat: Provide example of electron use in React app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
+// Vendor Assets
 import React, { Component } from 'react';
+
+// Project Assets
 import logo from './logo.svg';
 import './App.css';
+
+// Electron
+const electron = window.require('electron');
+const remote = electron.remote;
+const fs = remote.require('fs');
 
 class App extends Component {
   render() {
@@ -12,6 +20,12 @@ class App extends Component {
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
+
+          <br /><br />
+
+          We are using node {remote.process.versions.node},
+          Chrome {remote.process.versions.chrome},
+          and Electron {remote.process.versions.electron}.
         </p>
       </div>
     );
